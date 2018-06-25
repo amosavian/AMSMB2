6d6e77213794a41a250b7cb7ba75abf1951c832d
Amir Abbas <a.mosavian@gmail.com>
2018-06-26 04:14:05 +0430

    Revert "Removed openssl dependency"
    This reverts commit 5ed3390258144567831a699ae4aeee449a3b5574.

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 for i in "$@" ; do
+    if [[ $i == "--without-ssl" ]] ; then
+        WITHOUT_SSL="YES"
+        echo "Building without OpenSSL."
+        break
+    fi
+
     if [[ $i == "--with-libkrb5" ]] ; then
         WITH_KRB5="YES"
         echo "Building with Kerberos 5."
@@ -28,6 +34,12 @@ for pkg in cmake automake autoconf libtool; do
     fi
 done
 
+if [[ -z "${WITHOUT_SSL}" ]]; then
+    ./openssl-build.sh
+else
+    ./openssl-build.sh --without-ssl
+fi
+
 if [ ! -d libsmb2 ]; then
   git clone https://github.com/sahlberg/libsmb2
   cd libsmb2
@@ -39,7 +51,7 @@ fi
 
 export MINSDKVERSION=9.0
 export USECLANG=1
-export CFLAGS="-fembed-bitcode -DHAVE_SOCKADDR_LEN=1 -DHAVE_SOCKADDR_STORAGE=1"
+export CFLAGS="-fembed-bitcode -DHAVE_OPENSSL_LIBS=1 -DHAVE_SOCKADDR_LEN=1 -DHAVE_SOCKADDR_STORAGE=1"
 export CPPFLAGS="-I${PACKAGE_DIRECTORY}/buildtools/include"
 #export CPPFLAGS="-I/usr/local/opt/openssl/include"
 export LDFLAGS="-L${LIB_OUTPUT}"
---
+++ buildtools/openssl-build.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# This script builds the iOS and Mac openSSL libraries with Bitcode enabled
+# Download openssl http://www.openssl.org/source/ and place the tarball next to this script
+
+# Credits:
+# https://github.com/st3fan/ios-openssl
+# https://github.com/x2on/OpenSSL-for-iPhone/blob/master/build-libssl.sh
+# Peter Steinberger, PSPDFKit GmbH, @steipete.
+# Doron Adler, GlideTalk, @Norod78
+
+# Updated to work with Xcode 7 and iOS 9
+
+set -e
+
+###################################
+# 		 OpenSSL Version
+###################################
+OPENSSL_VERSION="openssl-1.0.2d"
+###################################
+
+###################################
+# 		 SDK Version
+###################################
+IOS_SDK_VERSION=$(xcodebuild -version -sdk iphoneos | grep SDKVersion | cut -f2 -d ':' | tr -d '[[:space:]]')
+###################################
+
+################################################
+# 		 Minimum iOS deployment target version
+################################################
+MIN_IOS_VERSION="9.0"
+
+echo "----------------------------------------"
+echo "OpenSSL version: ${OPENSSL_VERSION}"
+echo "iOS SDK version: ${IOS_SDK_VERSION}"
+echo "iOS deployment target: ${MIN_IOS_VERSION}"
+echo "----------------------------------------"
+echo " "
+
+DEVELOPER=`xcode-select -print-path`
+
+buildIOS()
+{
+	ARCH=$1
+	echo "Start Building ${OPENSSL_VERSION} for ${PLATFORM} ${IOS_SDK_VERSION} ${ARCH}"
+	
+	pushd . > /dev/null
+	cd "${OPENSSL_VERSION}"
+  
+	if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]]; then
+		PLATFORM="iPhoneSimulator"
+	else
+		PLATFORM="iPhoneOS"
+		sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
+	fi
+  
+	export $PLATFORM
+	export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
+	export CROSS_SDK="${PLATFORM}${IOS_SDK_VERSION}.sdk"
+	export BUILD_TOOLS="${DEVELOPER}"
+	export CC="${BUILD_TOOLS}/usr/bin/gcc -fembed-bitcode -mios-version-min=${MIN_IOS_VERSION} -arch ${ARCH}"
+	
+	echo "Configure"
+	if [[ "${ARCH}" == "x86_64" ]]; then
+		./Configure darwin64-x86_64-cc --openssldir="/tmp/${OPENSSL_VERSION}-iOS-${ARCH}" &> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log"
+	else
+		./Configure iphoneos-cross --openssldir="/tmp/${OPENSSL_VERSION}-iOS-${ARCH}" &> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log"
+	fi
+	# add -isysroot to CC=
+	sed -ie "s!^CFLAG=!CFLAG=-isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -mios-version-min=${MIN_IOS_VERSION} !" "Makefile"
+	echo "make"
+	make >> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log" 2>&1
+	echo "make install"
+	make install >> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log" 2>&1
+	echo "make clean"
+	make clean  >> "/tmp/${OPENSSL_VERSION}-iOS-${ARCH}.log" 2>&1
+	popd > /dev/null
+	
+	echo "Done Building ${OPENSSL_VERSION} for ${ARCH}"
+}
+
+for i in "$@" ; do
+    if [[ $i == "--without-ssl" ]] ; then
+        WITHOUT_SSL="YES"
+        break
+    fi
+done
+
+echo "Cleaning up"
+rm -rf include/openssl/* lib/*
+rm -rf /tmp/${OPENSSL_VERSION}-*
+rm -rf ${OPENSSL_VERSION}
+mkdir -p include/openssl/
+rm -rf "/tmp/${OPENSSL_VERSION}-*"
+rm -rf "/tmp/${OPENSSL_VERSION}-*.log"
+rm -rf "${OPENSSL_VERSION}"
+if [ ! -e ${OPENSSL_VERSION}.tar.gz ]; then
+	echo "Downloading ${OPENSSL_VERSION}.tar.gz"
+	curl -O https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz
+else
+	echo "Using ${OPENSSL_VERSION}.tar.gz"
+fi
+echo "Unpacking openssl"
+tar xfz "${OPENSSL_VERSION}.tar.gz"
+
+buildIOS "armv7"
+echo "Copying headers"
+cp /tmp/${OPENSSL_VERSION}-iOS-armv7/include/openssl/* include/openssl/
+if [[ -z "${WITHOUT_SSL}" ]]; then
+buildIOS "armv7s"
+buildIOS "arm64"
+buildIOS "x86_64"
+buildIOS "i386"
+
+echo "Building iOS libraries"
+lipo \
+	"/tmp/${OPENSSL_VERSION}-iOS-armv7/lib/libcrypto.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-armv7s/lib/libcrypto.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-arm64/lib/libcrypto.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-i386/lib/libcrypto.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-x86_64/lib/libcrypto.a" \
+	-create -output "${LIB_OUTPUT}/libcrypto.a"
+lipo \
+	"/tmp/${OPENSSL_VERSION}-iOS-armv7/lib/libssl.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-armv7s/lib/libssl.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-arm64/lib/libssl.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-i386/lib/libssl.a" \
+	"/tmp/${OPENSSL_VERSION}-iOS-x86_64/lib/libssl.a" \
+	-create -output "${LIB_OUTPUT}/libssl.a"
+fi
+
+echo "Cleaning up"
+rm -rf /tmp/${OPENSSL_VERSION}-*
+rm -rf ${OPENSSL_VERSION}
+echo "Done"
