2691c234c249a10fa93087d5c71f5a05fc19c594
Amir Abbas <a.mosavian@gmail.com>
2018-07-01 15:11:26 +0430

    Update libsmb2, laying foundaiton for multiplatform support

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
 for i in "$@" ; do
-    if [[ $i == "--without-ssl" ]] ; then
-        WITHOUT_SSL="YES"
-        echo "Building without OpenSSL."
-        break
-    fi
-
     if [[ $i == "--with-libkrb5" ]] ; then
         WITH_KRB5="YES"
         echo "Building with Kerberos 5."
@@ -34,12 +28,6 @@ for pkg in cmake automake autoconf libtool; do
     fi
 done
 
-if [[ -z "${WITHOUT_SSL}" ]]; then
-    ./openssl-build.sh
-else
-    ./openssl-build.sh --without-ssl
-fi
-
 if [ ! -d libsmb2 ]; then
   git clone https://github.com/sahlberg/libsmb2
   cd libsmb2
@@ -48,10 +36,10 @@ else
   cd libsmb2
 fi
 
-
+export OS=ios
 export MINSDKVERSION=9.0
 export USECLANG=1
-export CFLAGS="-fembed-bitcode -DHAVE_OPENSSL_LIBS=1 -DHAVE_SOCKADDR_LEN=1 -DHAVE_SOCKADDR_STORAGE=1"
+export CFLAGS="-fembed-bitcode -DHAVE_SOCKADDR_LEN=1 -DHAVE_SOCKADDR_STORAGE=1"
 export CPPFLAGS="-I${PACKAGE_DIRECTORY}/buildtools/include"
 #export CPPFLAGS="-I/usr/local/opt/openssl/include"
 export LDFLAGS="-L${LIB_OUTPUT}"
@@ -59,25 +47,15 @@ export LDFLAGS="-L${LIB_OUTPUT}"
 
 echo "Making libsmb2 static libararies"
 if [[ -z "${WITH_KRB5}" ]]; then
-    ../autoframework SMB2 libsmb2.a --without-libkrb5 --disable-werror > /dev/null
+    ../autoframework libsmb2.a --without-libkrb5 --disable-werror > /dev/null
 else
-    ../autoframework SMB2 libsmb2.a --disable-werror > /dev/null
+    ../autoframework libsmb2.a --disable-werror  > /dev/null
 fi
 
 cd ..
 
-echo "Building libsmb2 library"
-lipo \
-	"libsmb2/Static/arm64/lib/libsmb2.a" \
-    "libsmb2/Static/armv7/lib/libsmb2.a" \
-    "libsmb2/Static/armv7s/lib/libsmb2.a" \
-    "libsmb2/Static/i386/lib/libsmb2.a" \
-    "libsmb2/Static/x86_64/lib/libsmb2.a" \
-    -create -output "${LIB_OUTPUT}/libsmb2.a"
-
-echo  "Copying headers"
+echo  "Copying additional headers"
 cp    "libsmb2/include/libsmb2-private.h" "${PACKAGE_DIRECTORY}/libsmb2/include/"
-cp -R "libsmb2/include/smb2"              "${PACKAGE_DIRECTORY}/libsmb2/include/"
 cp    "module.modulemap"                  "${PACKAGE_DIRECTORY}/libsmb2/include/"
 
 rm -rf libsmb2
